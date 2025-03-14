b7cd2f6b4e26b25f13f8683322575a3adffbe962
ManojKumar-18-24 <manojkumarsamudrala836@gmail.com>
2025-03-14 17:32:15 +0530

    Add variable examples for number, boolean, and any types in TypeScript

--- 01INTRO/variables.ts
+++ 01INTRO/variables.ts
@@ -2,4 +2,22 @@ let greetings: string = "Hello ManojKumar" ;
 
 console.log(greetings)
 
+// number 
+
+let userId : number = 34 // too obvious don't need colon
+
+// boolean
+
+let isLoggedIn : boolean = false
+
+//any 
+
+let Hero:string
+
+function getHero(){
+    return "batman"
+}
+
+Hero = getHero()
+
 export {}
